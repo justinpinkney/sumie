2f3fdeaf1c27d9a92314f75d98201b0bc186458e
Justin <justinpinkney@gmail.com>
2019-01-09 22:53:11 +0000

    cppn

---
+++ cppn.py
@@ -0,0 +1,41 @@
+import numpy as np
+import torch
+
+def comp_activation(x, unbiased=True):
+    """composite activation from lucid cppn"""
+    x = torch.atan(x)
+    x1 = x/0.67
+    x2 = (x*x - 0.45)/0.396
+    return torch.cat((x1, x2), dim=1)
+
+class CPPN(torch.nn.Module):
+    """Implements a compositional pattern producing network."""
+
+    def __init__(self, num_layers, num_channels):
+        super(CPPN, self).__init__()
+        self.activation = comp_activation
+        self.multiplier = 2
+
+        self.inputLayer = torch.nn.Conv2d(2, num_channels, 1)
+        torch.nn.init.kaiming_normal_(self.inputLayer.weight, nonlinearity='linear')
+
+        self.innerLayer = torch.nn.ModuleList()
+        for i in range(num_layers):
+            conv_layer = torch.nn.Conv2d(self.multiplier*num_channels, num_channels, 1)
+            torch.nn.init.kaiming_normal_(conv_layer.weight, nonlinearity='linear')
+            self.innerLayer.append(conv_layer)
+
+        self.outputLayer = torch.nn.Conv2d(self.multiplier*num_channels, 3, 1)
+        torch.nn.init.zeros_(self.outputLayer.weight)
+
+    def forward(self, x):
+        
+        x = self.activation(self.inputLayer(x))
+        
+        for layer in self.innerLayer:
+            x = self.activation(layer(x))
+
+        x = self.outputLayer(x).sigmoid()
+        return x
+
+
