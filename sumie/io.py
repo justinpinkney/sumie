51ddd3db4d1fa51e4da225aa696031d1415c0ced
Justin <justinpinkney@gmail.com>
2019-02-10 21:54:24 +0000

    add io package

---
+++ sumie/io.py
@@ -0,0 +1,21 @@
+import torch
+from PIL import Image
+import numpy as np
+import requests
+from io import BytesIO
+
+
+def load_file(filename, size=None):
+    """Load an image from file as a tensor"""
+    im = Image.open(filename)
+    if size:
+        im = im.resize(size)
+    original_im = np.asarray(im)
+    target = torch.Tensor(original_im)
+    return target.permute(2,1,0).unsqueeze(0)
+
+def load_url(url, size=None):
+    """Load an image from url."""
+    response = requests.get(url)
+    contents = BytesIO(response.content)
+    return load_file(contents, size=size)
---
+++ tests/test_io.py
@@ -0,0 +1,26 @@
+import sumie.io
+
+from pathlib import Path
+
+def test_im_file():
+    """Load an image from file."""
+    filename = Path(__file__).with_name("tree.jpg")
+    image = sumie.io.load_file(filename)
+
+    assert image.size() == (1, 3, 296, 221)
+
+def test_im_file_resize():
+    """Load and resize an image."""
+    filename = Path(__file__).with_name("tree.jpg")
+    expected_size = (123, 234)
+    image = sumie.io.load_file(filename, size=expected_size)
+
+    assert image.size() == (1, 3,) + expected_size
+
+def test_im_url():
+    """Load an image from a url."""
+    url = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/606px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"
+    expected_size = (123, 234)
+    image = sumie.io.load_url(url, size=expected_size)
+
+    assert image.size() == (1, 3,) + expected_size
